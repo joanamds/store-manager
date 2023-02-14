f296499e2edc591f6421f0e165c57b0fe4a70c96
Joana <joanamdsantos1@gmail.com>
2023-02-14 11:43:35 -0300

    tentando testar o controller para encontrar o id

--- tests/unit/controllers/product.controller.test.js
+++ tests/unit/controllers/product.controller.test.js
@@ -6,7 +6,7 @@ const sinonChai = require('sinon-chai');
 const productService = require('../../../src/services/products.service');
 const productController = require('../../../src/controllers/product.controller');
 
-const { allProducts } = require('./mocks/product.controller.mock');
+const { allProducts, product } = require('./mocks/product.controller.mock');
 
 chai.use(sinonChai);
 
@@ -25,6 +25,28 @@ describe('Teste de unidade do controller de produtos', function () {
 
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(allProducts);
+  });
+
+  it('Deve retornar o produto de acordo com o id', async function () {
+    const res = {};
+    const req = {
+      params: { id: 1 },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productService, 'getProductById')
+      .resolves({ type: null, message: product });
+
+    await productController.listProducts(req, res);
+    
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(product);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
 
