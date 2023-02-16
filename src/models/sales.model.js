21a8eac2f3da0acbc01d1399c7e59c1a1a93ff96
Joana <joanamdsantos1@gmail.com>
2023-02-16 14:26:33 -0300

    6 - Crie endpoint para validar e cadastrar vendas

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -8,7 +8,7 @@ const insert = async () => {
   return insertId;
 };
 
-const insertProducts = async ({ id, productId, quantity }) => {
+const insertProducts = async (id, productId, quantity) => {
   const [[result]] = await connection.execute(
     'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
     [id, productId, quantity],
